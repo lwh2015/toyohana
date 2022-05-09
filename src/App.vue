ed393f393de47ba1cd37c9a0ba8a319ce1ee403f
lwh2015 <lingweiheng_2015@163.com>
2022-05-09 17:05:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <router-link to="/hxx">胡小雄</router-link
+  ><router-link to="/ljf">卢嘉枫</router-link
+  ><router-link to="/lm">李鸣</router-link
+  ><router-link to="/wch">王春桦</router-link
+  ><router-link to="/yjh">余佳豪</router-link>
+  <router-view></router-view>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import hxx from "./view/hxx/main.vue";
+import ljf from "./view/ljf/main.vue";
+import lm from "./view/lm/main.vue";
+import wch from "./view/wch/main.vue";
+import yjh from "./view/yjh/main.vue";
+
+const routes = [
+  { path: "/hxx", component: hxx },
+  { path: "/ljf", component: ljf },
+  { path: "/lm", component: lm },
+  { path: "/wch", component: wch },
+  { path: "/yjh", component: yjh },
+];
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp(App).use(router).mount("#app");
